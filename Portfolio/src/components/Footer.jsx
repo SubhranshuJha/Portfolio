7e4325fe55f5d0977f8e8d6b3b5c60917e3bab36
Subhranshu Jha <jhasubhranshu2005@gmail.com>
2025-07-13 17:36:54 +0530

    Portfolio

---
+++ Portfolio/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} Subhranshu Jha. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
