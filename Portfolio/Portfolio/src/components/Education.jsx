6869e208fc353365368cc930e7f13311238fac74
Subhranshu Jha <jhasubhranshu2005@gmail.com>
2025-07-13 18:35:55 +0530

    hm

---
+++ Portfolio/Portfolio/src/components/Education.jsx
@@ -0,0 +1,18 @@
+// src/components/Education.jsx
+import React from 'react';
+
+const Education = () => {
+  return (
+    <section id="education" className="education">
+      <h2>Education</h2>
+      <div className="edu-item">
+        <h3>B.Tech in Information Technology</h3>
+        <p>GEC Gandhinagar, Gujarat Technological University</p>
+        <p>2023 – 2027</p>
+        <p>CPI : 9.16 </p>
+      </div>
+    </section>
+  );
+};
+
+export default Education;
